d6a256a66021504e0bd65e24bf5a1036f4b107c4
mandriic <kellaxxx@gmail.com>
2022-08-12 10:19:21 +0200

    unknown changes in genkeys

--- gen_keypair.sh
+++ gen_keypair.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 openssl genrsa -des3 -out .private.pem 2048
 openssl rsa -in .private.pem -outform PEM -pubout -out .public.pem
-pyinstaller --onedir stockholm.py
-mkdir ./stockholm_src
-cp -r ./dist/stockholm/* ./stockholm_src/
-cp ./.public.pem ./stockholm_src
-cp ./.list_extenc_wannacry.txt ./stockholm_src
-cp -r ./.src ./stockholm_src
-tar -czvf stockholm.tar.gz ./stockholm_src
+cp .public.pem ./includes
+nuitka3  --standalone  --onefile --include-data-dir=./includes stockholm.py
+# tar -czvf stockholm.tar.gz ./stockholm_src
